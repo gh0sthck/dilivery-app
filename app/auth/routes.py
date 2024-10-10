2a889bc8a3901cff374ca96d29655ab8c78cea07
gh0st <maratka2202@gmail.com>
2024-10-10 18:53:55 +0300

    fixes

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -11,52 +11,46 @@ users_explorer = UserExplorer()
 http_bearer = HTTPBearer()
 
 
-async def validate_auth_user(
-    username: str = Form(),
-    password: str = Form()
-):
+async def validate_auth_user(username: str = Form(), password: str = Form()):
     unauth_exception = HTTPException(
-        status_code=401,
-        detail="Invalid username or password"
-    ) 
-    user = await users_explorer.get_by_username(username=username)  
+        status_code=401, detail="Invalid username or password"
+    )
+    user = await users_explorer.get_by_username(username=username)
     if not user:
         raise unauth_exception
-    if validate_password(password=password, hash=user.password): 
+    if validate_password(password=password, hash=user.password):
         return user
     raise unauth_exception
 
 
-async def get_current_user(creds: HTTPAuthorizationCredentials = Depends(http_bearer)) -> UserSchema:
+async def get_current_user(
+    creds: HTTPAuthorizationCredentials = Depends(http_bearer),
+) -> UserSchema:
     token: dict = decode_jwt(jwt_token=creds.credentials.encode())
-    current_user = await users_explorer.get_by_username(
-        username=token.get("username")
-    ) 
+    current_user = await users_explorer.get_by_username(username=token.get("username"))
     return current_user
 
 
 @users_router.post("/login/")
-async def user_login(schema: UserSchema = Depends(validate_auth_user)) -> JWTToken: 
-    schema.password = hash_password(schema.password.decode()).decode() 
+async def user_login(schema: UserSchema = Depends(validate_auth_user)) -> JWTToken:
+    schema.password = hash_password(schema.password.decode()).decode()
     payload = JWTPayload(
-        sub=schema.username,
-        username=schema.username,
-        email=schema.email
-    ) 
-    token = encode_jwt(payload=payload) 
-     
-    return JWTToken(
-        access_token=token
+        sub=schema.username, username=schema.username, email=schema.email
     )
+    token = encode_jwt(payload=payload)
+
+    return JWTToken(access_token=token)
 
 
 @users_router.post("/register/")
 async def user_register(schema: RegisterUser = Depends()) -> UserSchema:
-    schema.password = hash_password(schema.password).decode()  
-    user = await users_explorer.post(schema=schema) 
+    schema.password = hash_password(schema.password).decode()
+    user = await users_explorer.post(schema=schema)
     return user
 
 
 @users_router.post("/me/")
-async def user_current_user(schema: UserSchema = Depends(get_current_user)) -> UserSchema: 
+async def user_current_user(
+    schema: UserSchema = Depends(get_current_user),
+) -> UserSchema:
     return schema
