4b18c17c30c51966d4342060e9c7f3d2ebc6a87b
gh0st <maratka2202@gmail.com>
2025-03-13 17:42:41 +0300

    city autoincrement update

--- app/city/routes.py
+++ app/city/routes.py
@@ -2,27 +2,27 @@ from typing import Annotated, List, Optional
 from fastapi import APIRouter, Depends
 
 from app.city.models import City
-from app.city.schema import CitySchema
+from app.city.schema import CitySchema, CitySchemaRead
 from app.db_explorer import DbExplorer
 
 
 city_router = APIRouter(prefix="/api/city", tags=["City"])
-city_explorer = DbExplorer(model=City, schema=CitySchema)
+city_explorer = DbExplorer(model=City, schema=CitySchemaRead)
 
 
 @city_router.get("/all/")
-async def city_all() -> Optional[List[CitySchema]]:
+async def city_all() -> Optional[List[CitySchemaRead]]:
     return await city_explorer.get()
 
 
 @city_router.get("/{id}/")
-async def city_by_id(id: int) -> Optional[CitySchema]:
+async def city_by_id(id: int) -> Optional[CitySchemaRead]:
     return await city_explorer.get(id=id)
 
 
 @city_router.post("/add/")
 async def city_add(
-    schema: Annotated[CitySchema, Depends()],
+    schema: CitySchema,
 ) -> CitySchema:
     return await city_explorer.post(schema=schema)
 
@@ -30,11 +30,11 @@ async def city_add(
 @city_router.put("/update/{id}/")
 async def city_update(
     id: int,
-    schema: Annotated[CitySchema, Depends()],
-) -> Optional[CitySchema]:
+    schema: CitySchema,
+) -> Optional[CitySchemaRead]:
     return await city_explorer.update(id=id, schema=schema)
 
 
 @city_router.delete("/delete/{id}")
-async def city_delete(id: int) -> Optional[CitySchema]:
+async def city_delete(id: int) -> Optional[CitySchemaRead]:
     return await city_explorer.delete(id=id)
--- app/city/schema.py
+++ app/city/schema.py
@@ -2,5 +2,8 @@ from pydantic import BaseModel, Field
 
 
 class CitySchema(BaseModel):
-    id: int
     name: str = Field(max_length=80)
+
+
+class CitySchemaRead(CitySchema):
+    id: int
